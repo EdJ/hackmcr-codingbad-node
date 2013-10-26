f4de68666fa1c2ca6c0a314a1533f8c3d6370305
Srinivas Sunkari <sr.sunkari@gmail.com>
2013-10-26 21:05:18 +0100

    Leader Board Added test to assert on player scores

--- test/testLeaderboard.js
+++ test/testLeaderboard.js
@@ -1,12 +1,18 @@
 require('chai').should();
 var leaderboard = require('../lib/leaderboard');
 
-describe('leaderboard', function () {
-	it('connect emits initial player data', function(done) {
-		leaderboard.connect(function(playerData) {
-			playerData.id.should.not.be.null;
-			playerData.leaderBoard.should.not.be.null;
-			done();
-		});
-	})
+describe('leaderboard', function() {
+    it('connect emits initial player data', function(done) {
+        leaderboard.connect(function(playerData) {
+            playerData.id.should.not.be.null;
+            playerData.leaderBoard.should.not.be.null;
+            done();
+        });
+    });
+    it('update leaderboard with player player score', function() {
+        leaderboard.add_score(function(score) {
+            score.id.should.not.be.null;
+            expect(score.score).not.null;
+        });
+    })
 });
