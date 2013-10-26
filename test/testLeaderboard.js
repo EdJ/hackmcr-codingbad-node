e2c70b4aecdf4f6087d5ae574aaa23760d4a038e
Srinivas Sunkari <sr.sunkari@gmail.com>
2013-10-26 21:33:15 +0100

    Added Test to update the player score to leader board

--- test/testLeaderboard.js
+++ test/testLeaderboard.js
@@ -6,13 +6,40 @@ describe('leaderboard', function() {
         leaderboard.connect(function(playerData) {
             playerData.id.should.not.be.null;
             playerData.leaderBoard.should.not.be.null;
+
             done();
         });
     });
-    it('update leaderboard with player player score', function() {
-        leaderboard.add_score(function(score) {
-            score.id.should.not.be.null;
-            expect(score.score).not.null;
+    it('update leaderboard with player score', function(done) {
+        var firstFire = true;
+        leaderboard.connect(function(playerData) {
+            if (firstFire) {
+                firstFire = false;
+                return;
+            }
+
+            var hadItem = false;
+            for (var i = playerData.leaderBoard.length; i--;) {
+                var value = playerData.leaderBoard[i];
+
+                if (value.id == 1) {
+                    value.should.eql({
+                        id: 1,
+                        score: 20
+                    });
+
+                    hadItem = true;
+                }
+            }
+
+            hadItem.should.equal(true);
+
+            done();
+        });
+
+        leaderboard.add_score({
+            id: 1,
+            score: 20
         });
     })
 });
