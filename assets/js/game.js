677e63e34606a33dca4a11258e21b6264226404a
AndyLowryLateRooms <andrew.lowry@laterooms.com>
2013-10-27 13:23:12 +0000

    Add tweet my score button

--- assets/js/game.js
+++ assets/js/game.js
@@ -272,10 +272,17 @@ endGame = function() {
 
     showFailedBanner();
 
+    setTwitterTweet();
+
     if ($.QueryString.chimput) {
         setTimeout(function() { location.reload(); }, 5000);
     }
 };
+
+var setTwitterTweet = function() {
+    $("#tweetButton").attr("data-text", "I've just scored " + score + " on Travelator. Can you beat that?");
+    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+}
 
 var onTick = function(event) {
     if (stopUpdating) {
