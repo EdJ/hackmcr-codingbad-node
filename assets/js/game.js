cff1a26b372a49edf9b87793b0743f3996f93158
fractiva <wongcheming@gmail.com>
2013-10-27 02:23:35 +0000

    corrected security man finally

--- assets/js/game.js
+++ assets/js/game.js
@@ -260,7 +260,7 @@ var createSecurityAvatar = function() {
         },
         // define two animations, run (loops, 1.5x speed) and jump (returns to run):
         "animations": {
-            "run": [11, 15, "run", 0.5]
+            "run": [12, 15, "run", 0.5]
         }
     });
 
