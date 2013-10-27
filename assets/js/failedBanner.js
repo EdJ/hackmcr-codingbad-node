a05a7e861d3b198695c8c28c921fa4a0502784f9
AndyLowryLateRooms <andrew.lowry@laterooms.com>
2013-10-27 12:02:45 +0000

    Add refresh your browser message

--- assets/js/failedBanner.js
+++ assets/js/failedBanner.js
@@ -3,6 +3,11 @@ var showFailedBanner = function() {
     asset.setTransform(0, 0, scale, scale);
     asset.y = viewport.dimensions.y / 5;
     asset.x = viewport.dimensions.x / 5;
+    stage.addChild(asset);
 
+    var asset = new createjs.Text("Refresh your browser to play again.", "32px Arial", "red");
+    asset.setTransform(0, 0, scale, scale);
+    asset.y = (viewport.dimensions.y / 5) * 2;
+    asset.x = viewport.dimensions.x / 5;
     stage.addChild(asset);
 };
