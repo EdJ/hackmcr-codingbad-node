46f58dae777899dcb8a7a81c1a4239841fbb45b4
fractiva <wongcheming@gmail.com>
2013-10-27 03:36:06 +0000

    Split out entity.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,61 +14,6 @@ var scale = 1;
 
 var groundLevel = 0;
 
-var Entity = function Entity() {
-    this._velocity = new Vector();
-
-    this._acceleration = new Vector();
-
-    this._position = new Vector();
-    this._maxVelocity = new Vector(100, 100);
-    this._dimensions = new Vector();
-};
-
-Entity.prototype.setVelocity = function(newVelocity) {
-    this._velocity = newVelocity;
-};
-
-Entity.prototype.setMaxVelocity = function(maxVelocity) {
-    this._maxVelocity = maxVelocity;
-};
-
-Entity.prototype.setAcceleration = function(newAcceleration) {
-    this._acceleration = newAcceleration;
-};
-
-Entity.prototype.setPosition = function(newPosition) {
-    this._position = newPosition;
-};
-
-Entity.prototype.setDimensions = function(newDimensions) {
-    this._dimensions = newDimensions;
-};
-
-Entity.prototype.update = function() {
-    this._position.add(this._velocity.multiply(scale).multiply(fpsHandler.frameComplete));
-
-    this.asset.x = this._position.x;
-    this.asset.y = this._position.y;
-
-    this._velocity.add(this._acceleration).cap(this._maxVelocity);
-};
-
-Entity.prototype.startScrolling = function() {
-    this._oldUpdate = this.update;
-
-    this.update = function() {
-        this._oldUpdate();
-
-        if (this._position.x < -this._dimensions.x) {
-            this._position.x += this._dimensions.x;
-        }
-    };
-};
-
-Entity.prototype.stopScrolling = function() {
-    this.update = this._oldUpdate;
-};
-
 var setupGame = function(stage) {
     canvas = stage.canvas;
 
