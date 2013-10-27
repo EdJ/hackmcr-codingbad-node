a68b4e8118651a3605c36f181bd1198b443c21f7
fractiva <wongcheming@gmail.com>
2013-10-27 03:55:24 +0000

    split out background

--- assets/js/game.js
+++ assets/js/game.js
@@ -198,33 +198,6 @@ var createSecurityAvatar = function() {
     return avatar;
 };
 
-
-var createBackground = function() {
-    var background = new Entity();
-    var backgroundImage = loader.getResult("background");
-
-    var preScale = 1 / (((100 / groundLevel) * backgroundImage.height) / 100);
-
-    var asset = background.asset = new createjs.Shape();
-    var matrix = new createjs.Matrix2D
-    matrix.scale(preScale, preScale);
-
-    asset.graphics.beginBitmapFill(backgroundImage, 'repeat', matrix).drawRect(0, 0, viewport.dimensions.x + backgroundImage.width * preScale, backgroundImage.height * preScale);
-    asset.setTransform(0, 0, scale, scale);
-
-    background.setDimensions(new Vector(backgroundImage.width, backgroundImage.height).multiply(preScale));
-    background.setVelocity(new Vector(-3, 0));
-    background.startScrolling();
-
-    stage.addChild(asset);
-
-    entities.push(background);
-
-    return background;
-};
-
-
-
 var fpsHandler = {
     fps: 20,
     lastRender: 0,
